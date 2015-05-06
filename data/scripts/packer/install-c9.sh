d76732d99de7ef92849df3d7c80e50b4368b67cc
Nic Ashton <nic.ashton@theuniprogroup.com>
2015-05-06 15:18:04 +0100

    Brought in several fixes from php dev box.

--- data/scripts/packer/install-c9.sh
+++ data/scripts/packer/install-c9.sh
@@ -2,6 +2,10 @@
 
 sudo apt-get -y install tmux
 
+su vagrant <<'EOF'
+USERNAME=$(whoami)
+echo "Current user: $USERNAME"
+
 export NVM_DIR="/usr/local/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
@@ -10,10 +14,12 @@ node --version
 
 cd /home/vagrant
 git clone https://github.com/c9/core.git c9sdk
-chown vagrant:vagrant c9sdk -R
+sudo chown vagrant:vagrant c9sdk -R
+sudo chown vagrant:vagrant /home/vagrant/.npm/_locks -R
 
 cd c9sdk
 scripts/install-sdk.sh
+EOF
 
 touch /home/vagrant/startups/cloud9
 chmod +x /home/vagrant/startups/cloud9
